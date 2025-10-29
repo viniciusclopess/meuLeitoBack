6cfb40e34b0bbcad07f8b4a959a2c7888d32a336
Vinicius Lopes <Viniciusclopes2020@gmail.com>
2025-10-29 01:38:57 -0300

    feat(joins: ProfissionalPermissao)

--- src/routes/joinsRoutes.js
+++ src/routes/joinsRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const { postPacienteLeito, getPacienteLeito, putPacienteLeito } = require('../controllers/joinsController');
+const { 
+    postPacienteLeito, getPacienteLeito, putPacienteLeito, 
+    postProfissionalPermissao, getProfissionalPermissao, putProfissionalPermissao, deleteProfissionalPermissao
+} = require('../controllers/joinsController');
 
 const router = express.Router();
 
 router.post('/alocacao/', postPacienteLeito);
 router.get('/alocacao/', getPacienteLeito);
 router.put('/alocacao/:id', putPacienteLeito);
+
+router.post('/permissao/', postProfissionalPermissao);
+router.get('/permissao/', getProfissionalPermissao);
+router.put('/permissao/:id', putProfissionalPermissao);
+router.delete('/permissao/:id', deleteProfissionalPermissao);
 
 module.exports = router;
